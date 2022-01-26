75a2c857de6380263facc9baefcfd455b60c2933
Walter <silvawalteralipio@gmail.com>
2022-01-25 21:38:16 -0300

    arquivo reset

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+function GlobalStyle(){
+    return (
+        <style global jsx>{`
+        @import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,700;1,400&family=New+Tegomin&display=swap');
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                list-style: none;
+            }
+            body {
+                font-family: 'Open Sans', sans-serif;
+            }
+            /* App fit Height */ 
+            html, body, #__next {
+                min-height: 100vh;
+                display: flex;
+                flex: 1;
+            }
+            #__next {
+                flex: 1;
+            }
+            #__next > * {
+                flex: 1;
+            }
+            /* ./App fit Height */ 
+        `}</style>
+    );
+}
+
+export default function MyApp({ Component, pageProps }) {
+    //tudo que for carregado aqui, roda em todas as páginas
+    return <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+  }
